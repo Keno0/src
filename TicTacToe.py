8134b30be549fcbf3f4337705e8d259df88a9edb
Keno0 <norby185@gmail.com>
2017-03-15 11:18:23 +0000

    Create Board

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,1 +1,17 @@
+import pprint
 
+#get size of the board
+print('Please give the size of the board.')
+sizeOfBoard = int(input())
+TicTacToeBoard = {}
+
+#create board
+def createBoard(dimension, board):
+    for i in range(dimension):
+        for j in range(dimension):
+            board[str(i) + str(j)] = ' '
+            
+
+createBoard(sizeOfBoard, TicTacToeBoard )        
+
+pprint.pprint(TicTacToeBoard)
